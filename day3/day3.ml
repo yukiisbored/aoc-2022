ff74770a6fb33cfc5ba4a7adee01ed387e779946
Yuki Langley <hi@yukiisbo.red>
2022-12-03 17:07:20 +0100

    Solve Day 3

---
+++ day3/day3.ml
@@ -0,0 +1,47 @@
+open Core
+
+let to_prio_int = function
+  | x when Char.(x < 'a') -> Char.(to_int x - to_int 'A') + 27
+  | x -> Char.(to_int x - to_int 'a') + 1
+
+let part1 lines =
+  let f line =
+    let xs = String.to_list line in
+    let ls, rs = List.split_n xs (List.length xs / 2) in
+    let ls', rs' =
+      (Set.of_list (module Char) ls, Set.of_list (module Char) rs)
+    in
+    Set.inter ls' rs' |> Set.choose_exn |> to_prio_int
+  in
+  List.map ~f lines |> List.fold ~init:0 ~f:( + ) |> string_of_int
+
+let part2 lines =
+  let grouper (acc : string list list) (line : string) =
+    let t = List.hd_exn acc in
+    if Int.(List.length t = 3) then [ line ] :: acc
+    else (line :: t) :: List.tl_exn acc
+  in
+  List.fold ~init:[ [] ] ~f:grouper lines
+  |> List.map
+       ~f:(List.map ~f:(fun x -> String.to_list x |> Set.of_list (module Char)))
+  |> List.map ~f:(function
+       | [ x; y; z ] ->
+           Set.inter x y |> Set.inter z |> Set.choose_exn |> to_prio_int
+       | _ -> failwith "unreachable")
+  |> List.fold ~init:0 ~f:( + )
+  |> string_of_int
+
+let solvercmd summary solver =
+  Command.basic ~summary
+    Command.Let_syntax.(
+      let%map_open filename = anon ("filename" %: string) in
+      fun () -> In_channel.read_lines filename |> solver |> print_endline)
+
+let command =
+  Command.group ~summary:"Advent of Code 2022"
+    [
+      ("part1", solvercmd "Solve Part 1" part1);
+      ("part2", solvercmd "Solve Part 2" part2);
+    ]
+
+let () = Command_unix.run command
